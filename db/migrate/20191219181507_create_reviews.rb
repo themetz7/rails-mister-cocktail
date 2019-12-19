9c40caed84fb32bae5dfd030799e3a0f1d2db63a
Lukas Metzmacher <lukas.metzmacher@bild.de>
2019-12-19 20:45:56 +0100

    db

--- db/migrate/20191219181507_create_reviews.rb
+++ db/migrate/20191219181507_create_reviews.rb
@@ -1,7 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-
+      t.text :content
+      t.integer :rating
+      t.references :cocktail, foreign_key: true
       t.timestamps
     end
   end
